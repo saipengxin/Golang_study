ef6d3b72ba4636002ae67d86969306e95b75f681
saipengxin <saipengxin@slodon.cn>
2021-11-12 11:21:53 +0800

    变量赋值

--- day01/02var/main.go
+++ day01/02var/main.go
@@ -1,1 +1,14 @@
-package _2var
+package main
+
+import "fmt"
+
+func foo() (int, string) {
+	return 10, "saipx"
+}
+
+func main() {
+	x, _ := foo()
+	_, y := foo()
+	fmt.Println("x=", x)
+	fmt.Println("y=", y)
+}
