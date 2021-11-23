0db18ce6447de10da5e6538fd8d45deec9fec48f
saipengxin <saipengxin@slodon.cn>
2021-11-23 14:44:57 +0800

    continue switch goto

---
+++ day02/04continue/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main()  {
+	XX:
+	for i := 0; i < 10; i++ {
+		for j := 0; j < 10; j++ {
+			if j == 2 {
+				continue XX
+			}
+			fmt.Printf("%d - %d\n",i , j)
+		}
+	}
+}
---
+++ day02/05switch/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main()  {
+	s := "a"
+	switch {
+	case s == "a":
+		fmt.Println("a")
+		fallthrough
+	case s == "b":
+		fmt.Println("b")
+	case s == "c":
+		fmt.Println("c")
+	default:
+		fmt.Println("...")
+	}
+}
