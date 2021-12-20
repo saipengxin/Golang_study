136ccd2ef8bd09607615c96a22c561025b1fd5cd
saipengxin <saipengxin@slodon.cn>
2021-12-20 20:51:02 +0800

    函数

--- day02/15func/main.go
+++ day02/15func/main.go
@@ -2,35 +2,18 @@ package main
 
 import "fmt"
 
-/**
-x 参数 int类型
-y 参数 int类型
-res 返回值 int类型
-*/
-func sum(x int, y int) (res int) {
-	return x + y
-}
-
-// 多个返回值
-func cheng(x int) (res1 int, res2 int) {
-	return x - 1, x - 2
-}
-
 func main() {
-	// 直接使用函数名调用并使用变量接收返回值
-	r := sum(1, 2)
-	// 返回多个返回值时，使用多个变量接收
-	i, j := cheng(5)
-	// 如果存在返回值不需要使用，使用匿名函数来接收
-	_, v := cheng(4)
-	// 调用有返回值函数的时候，我们也可以不接收返回值
-	cheng(6)
-	fmt.Println(r)
-	fmt.Println(i)
-	fmt.Println(j)
-	fmt.Println(v)
-}
+	var str = "上海自来水来自海上"
 
-func intSum(int, int) () {
+	// 先将字符串转为切片,否则无法使用for循环遍历出汉字来
+	var str_slice = []rune(str)
 
+	// 遍历切片
+	for i := 0; i < len(str_slice)/2; i++ {
+		if str_slice[i] != str_slice[len(str_slice)-1-i] {
+			fmt.Println("不是回文")
+		} else {
+			fmt.Println("是回文")
+		}
+	}
 }
