7a2f84c58a9b98b0f44b564d30ca7d5486ce14cc
saipengxin <saipengxin@slodon.cn>
2021-12-28 19:26:16 +0800

    结构体

--- day04/02struct/main.go
+++ day04/02struct/main.go
@@ -2,34 +2,34 @@ package main
 
 import "fmt"
 
-type person struct {
+//Animal 动物
+type Animal struct {
 	name string
-	city string
-	age  int
 }
 
-// 构造函数
-func NewPerson(name, city string, age int) *person {
-	return &person{
-		name: name,
-		city: city,
-		age:  age,
-	}
+// Animal指针类型的接收者，这个方法只有Animal类型的变量才能调用
+func (a *Animal) move() {
+	fmt.Printf("%s会动！\n", a.name)
 }
 
-//方法
-// 前面这个括号是指定的接收者，一般用接收者类型的第一个字母命名
-func (p person) Eating() {
-	fmt.Printf("%s正在吃饭", p.name)
+//Dog 狗
+type Dog struct {
+	Feet    int8
+	*Animal //通过嵌套匿名字段实现继承，此时Dog不光继承了Animal结构体中的参数，还继承了他的方法
+}
+
+func (d *Dog) wang() {
+	// 这里先在Dog中找name，没有找到会去嵌套的匿名字段中找name，也就是Animal
+	fmt.Printf("%s会汪汪汪~\n", d.name)
 }
 
 func main() {
-	p := NewPerson("saipx", "北京", 18)
-	fmt.Println(p)
-	// 调用方法，因为指定了接收者为person类型，所以只有person类型的变量才能调用
-	p.Eating()
-
-	// 不能像普通方法一样直接调用
-	// Eating()
-
+	d1 := &Dog{
+		Feet: 4,
+		Animal: &Animal{ //注意嵌套的是结构体指针
+			name: "乐乐",
+		},
+	}
+	d1.wang() //乐乐会汪汪汪~
+	d1.move() //乐乐会动！
 }
