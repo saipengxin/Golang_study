6464a37ac044e0308711ae326995d5507023a843
saipengxin <saipengxin@slodon.cn>
2022-01-06 09:08:03 +0800

    日志

--- day04/mylog_test/main.go
+++ day04/mylog_test/main.go
@@ -3,8 +3,10 @@ package main
 import "github.com/saipengxin/study/day04/mylog"
 
 func main() {
-	logger := mylog.NewLogger()
-	logger.Info("Info等级的日志")
+	//logger := mylog.NewConsoleLogger("Debug")
+	logger := mylog.NewFileLogger("Debug", "./", "demoLog.log", 10*1024*1024)
+	var err = "123报错了"
+	logger.Debug("Debug等级的日志,报错信息为:%v", err)
 	logger.Trace("Trace等级的日志")
 	logger.Info("Info等级的日志")
 	logger.Warning("Warning等级的日志")
